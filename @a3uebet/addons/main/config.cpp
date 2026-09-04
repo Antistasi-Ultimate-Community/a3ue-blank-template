7a55a7b7c86413348b494a3e9d2f287953f99e35
Oliver Schieche <post@oliver-schieche.de>
2026-09-04 08:28:29 +0200

    Template moved; make room for contrib files

---
+++ @a3uebet/addons/main/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3A_ultimate"};
+        author = LSTRING(Extender_Author);
+        authors[] = {};
+        authorUrl = LSTRING(Extender_AuthorUrl);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
