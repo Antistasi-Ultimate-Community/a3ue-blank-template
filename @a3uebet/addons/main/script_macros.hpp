8b4fbfd4687e3a49553a39cfd00033efd439ac93
Oliver Schieche <post@oliver-schieche.de>
2026-09-14 13:00:19 +0200

    Add A3U compatibility macros

--- @a3uebet/addons/main/script_macros.hpp
+++ @a3uebet/addons/main/script_macros.hpp
@@ -1,3 +1,10 @@
+// Our custom VERSION_CONFIG macro that also declares compatibility
+// with Antistasi Ultimate
+#define VERSION_CONFIG version = VERSION; \
+    versionStr = QUOTE(VERSION_STR); \
+    versionAr[] = {VERSION_AR}; \
+    A3A_compatibility[] = {A3A_COMPAT_MAJOR, A3A_COMPAT_MINOR}
+    
 // Load CBA headers
 #include "\x\cba\addons\main\script_macros.hpp"
 
--- @a3uebet/addons/main/script_mod.hpp
+++ @a3uebet/addons/main/script_mod.hpp
@@ -13,6 +13,21 @@
 // MINIMAL Arma version required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 2.22
 
+/* Antistasi Ultimate compatibility
+ *
+ * Requires Antistasi Ultimate version 12.0 or higher
+ *
+ * Your extender _MAY_ declare compatibility with a major release of Antistasi
+ * Ultimate by leaving the required minor version at 0. If, however, there is
+ * an update to A3U to e.g. version 12.1, your extender will be considered
+ * compatible, but still shown to administrators.
+ *
+ * The make sure your extender does not pop up in the extenders incompatibility
+ * notice dialog, keep these numbers up to date to A3U's current MAJOR.MINOR version.
+ */
+#define A3A_COMPAT_MAJOR 12
+#define A3A_COMPAT_MINOR 0
+
 #ifdef COMPONENT_BEAUTIFIED
     #define COMPONENT_NAME QUOTE(a3uebet - COMPONENT_BEAUTIFIED)
 #else
